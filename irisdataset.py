dab7abb0377191e4ea3c4300a32412d7d8ff1f59
Hudsonsue <G00219132@gmit.ie>
2018-04-03 19:34:32 +0100

    groupby added and tested on sepal dimensions

--- irisdataset.py
+++ irisdataset.py
@@ -6,6 +6,29 @@
 import numpy as np
 import pandas as pd
 import csv
-data = pd.read_csv("iris.csv", header=-1)
+data = pd.read_csv("iris.csv", header=-1) 
+#set header =-1 as data in row 0
 A = np.array (data)
+
+
+
+sum_col = (np.sum(A[:,0]))
+avg_col = sum_col/150
+
 print (data)
+print (np.min(A[:,0]))
+print (np.max(A[:,0]))
+print (np.sum(A[:,0]))
+print (avg_col)
+
+print (np.min(A[:,1]))
+print (np.max(A[:,1]))
+
+data.groupby(4)[0].min()
+print("minimum sepal length per speices" , data.groupby (4) [0].min())
+
+print("maximum sepal length per speices" , data.groupby (4) [0].max())
+
+print("mean sepal length per speices" , data.groupby (4) [0].mean())
+
+
