4cd17bc1db756bce77e6449a7b3429b3cbca579c
Hudsonsue <G00219132@gmit.ie>
2018-04-03 22:45:10 +0100

    using describe for summary data

--- irisdataset.py
+++ irisdataset.py
@@ -11,8 +11,6 @@ data = pd.read_csv("iris.csv", header=-1)
 #set header =-1 as data in row 0
 A = np.array (data)
 
-
-
 sum_col = (np.sum(A[:,0]))
 avg_col = sum_col/150
 
@@ -26,15 +24,24 @@ print (np.min(A[:,1]))
 print (np.max(A[:,1]))
 
 data.groupby(4)[0].min()
-print("minimum sepal length per speices" , data.groupby (4) [0].min())
+#
+print("minimum sepal length per species" , data.groupby (4) [0].min())
 
-print("maximum sepal length per speices" , data.groupby (4) [0].max())
+print("maximum sepal length per species" , data.groupby (4) [0].max())
 
-print("mean sepal length per speices" , data.groupby (4) [0].mean())
+print("mean sepal length per species" , data.groupby (4) [0].mean())
 
 
 #plt(data.groupby (4) [0].min())
 
 print (data[4].value_counts())
+
+summary = data.groupby(4)[0].describe()
+#temp.to_csv('out.csv')
+#
+print (summary)
+
+
+
 
 
