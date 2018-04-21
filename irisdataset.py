14240eb939ece27caff4c9a5025a5227986e9087
Hudsonsue <G00219132@gmit.ie>
2018-04-21 16:02:41 +0100

    save plots

--- irisdataset.py
+++ irisdataset.py
@@ -11,6 +11,7 @@ data = pd.read_csv("iris.csv", header=-1)
 #set header =-1 as data in row 0
 A = np.array (data)
 
+
 sum_col = (np.sum(A[:,0]))
 avg_col = sum_col/150
 
@@ -40,13 +41,27 @@ print (data[4].value_counts())
 summary = data.groupby(4)[0].describe()
 
 
-print (summary)
+print (" summary of data set", summary)
 summary.to_csv('summary.csv')
 
+# Below uses  matplotlib.pyplot to produce scatter graphs
+
+# Sepal characteristics
 x, y =[A[:,0],A[:,1]] 
+plt.xlabel('Sepal Length', fontsize = 14)
+plt.ylabel("Sepal Width", fontsize = 14)
+plt.title("Iris Datset: Sepal Charateristics", fontsize = 18)
 plt.scatter(x, y)
+plt.savefig ('Sepal.png')
 plt.show ()
+## note: must save before show as otherwise blank file!
 
-
-
+# Petal charateristics
+x, y =[A[:,2],A[:,3]] 
+plt.xlabel('Petal Length', fontsize = 14)
+plt.ylabel("Petal Width", fontsize = 14)
+plt.title("Iris Datset: Petal Charateristics", fontsize = 18)
+plt.scatter(x, y)
+plt.savefig ('Petal.png')
+plt.show ()
 
