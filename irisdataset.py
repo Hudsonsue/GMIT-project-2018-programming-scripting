5b0abdcd273dcedbe3d9811046a8247ecf9685b0
Hudsonsue <G00219132@gmit.ie>
2018-04-22 20:46:32 +0100

    further work

--- irisdataset.py
+++ irisdataset.py
@@ -7,6 +7,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import csv
+
+
 data = pd.read_csv("iris.csv", header=-1) 
 #set header =-1 as data in row 0
 A = np.array (data)
@@ -16,7 +18,7 @@ avg_col = sum_col/150
 
 print (data)
 
-f = open('generalstats.txt','w')
+#f = open('generalstats.txt','w')
 
 print('\n'*2)
 
@@ -61,6 +63,9 @@ print (data[4].value_counts())
 
 print('\n')
 
+# below uses the describe function to produce some statistics of the Iris dataset
+# it is displayed and also saved to csv file summary.csv
+
 summary = data.groupby(4)[0].describe()
 
 print('\n')
@@ -70,7 +75,8 @@ summary.to_csv('summary.csv')
 
 # Below uses  matplotlib.pyplot to produce and save scatter graphs
 
-##plt.ioff()###turns off interactieve display
+   
+## plt.ioff()
 
 # Sepal characteristics
 x, y =[A[:,0],A[:,1]] 
@@ -79,16 +85,37 @@ plt.ylabel("Sepal Width", fontsize = 14)
 plt.title("Iris Datset: Sepal Charateristics", fontsize = 18)
 plt.scatter(x, y)
 plt.savefig ('Sepal.png')
-##plt.show ()
+plt.show ()
 ## note: must save before show as otherwise blank file!
 
 # Petal charateristics
-x, y =[A[:,2],A[:,3]] 
-plt.colors 
+
+for i in range (0,len(A)-1):
+    x, y =[A[i][2],A[i][3]] 
+    ##flower = flower_type[i].decode("utf-8")
+	##color = ""
+
+    flower = A[i][4]
+    color = ""
+    if flower=='Iris-setosa':
+        color ="red"
+    elif flower=='Iris-versicolor':
+        color="blue"
+    elif flower=='Iris-virginica':
+        color = "green"
+        print (flower)
+        print(x,y)
+   
+    plt.scatter(x, y)
+
+#plt.colors(A[:,4]=="Iris-versicolor",color="blue
+# ")
+#plt.colors(A[:,4]=="Iris-virginica",color="red")
 plt.xlabel('Petal Length', fontsize = 14)
 plt.ylabel("Petal Width", fontsize = 14)
 plt.title("Iris Datset: Petal Charateristics", fontsize = 18)
-plt.scatter(x, y)
-plt.savefig ('Petal.png')
-##plt.show ()
+#    plt.scatter(x, y)
+##plt.savefig ('Petal.png')
+plt.show ()
+
 
