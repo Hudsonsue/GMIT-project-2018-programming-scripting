2344a7d83d2b687a1c6cd68037358744dda4fe0b
Hudsonsue <G00219132@gmit.ie>
2018-04-23 21:50:52 +0100

    seaborn plot file

--- seabornplots.py
+++ seabornplots.py
@@ -1,12 +1,21 @@
+# Programming and Scripting -iris dataset analysis 
+# G00219132 Susan Hudson 
+# this explores the use of seaborn to produce plots from the Iris dataset.
+# it uses the seaborn statistical data visualization library based on mathplotlib.
+# https://seaborn.pydata.org/ and https://seaborn.pydata.org/tutorial.html proved useful sources of information
 
+
+# import libraries
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 plt.style.use('seaborn-whitegrid')
-# library & dataset
-import seaborn as sns
+
+# import dataset
 df = sns.load_dataset('iris')
+
 print(df)
+
 # use the function regplot to make a scatterplot
 sns.regplot(x=df["sepal_length"], y=df["sepal_width"])
 plt.show()
@@ -17,33 +26,4 @@ sns.lmplot( x="sepal_length", y="sepal_width", data=df, fit_reg=False, hue='spec
 # Move the legend to an empty part of the plot
 plt.legend(loc='lower right')
 plt.show()
-
- # Petal charateristics
-
-for i in range (0,len(df)-1):
-    x, y =[df[i][2],df[i][3]] 
-    ##flower = flower_type[i].decode("utf-8")
-	##color = ""
-
-    flower = df[i][4]
-    
-	#flower = flower_type[i].decode("utf-8")
-    #color = ""
-    if flower=='Iris-setosa':color ="red"
-    elif flower =='Iris-versicolor':color="blue"
-    elif flower =='Iris-virginica':color = "green"
-      #  print (flower)
-       # print(x,y)
-   
-    sns.scatter(x, y)
-
-#plt.colors(A[:,4]=="Iris-versicolor",color="blue")
-#plt.colors(A[:,4]=="Iris-virginica",color="red")
-plt.xlabel('Petal Length', fontsize = 14)
-plt.ylabel("Petal Width", fontsize = 14)
-plt.title("Iris Datset: Petal Charateristics", fontsize = 18)
-plt.scatter(x, y)
-plt.savefig ('Petal.png')
-plt.show ()
-
 
