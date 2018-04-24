abeace2d657d65b3e7fad9e8ee228db1cf148a22
Hudsonsue <G00219132@gmit.ie>
2018-04-24 17:26:27 +0100

    ading some counts

--- irisdataset.py
+++ irisdataset.py
@@ -2,7 +2,9 @@
 # G00219132 Susan Hudson 
 # This script opens, reads and prints the Iris dataset csv file 
 # it will form the basis of the statistical analysis
+# note - throughout I have used print statements to create empty lines, single, double and multiples
 
+#Import libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -11,6 +13,17 @@ import sys
 
 ## below allows anyone running script to decide whether to save the print output to file or display in terminal
 
+# select plot style
+plt.style.use('seaborn-whitegrid')
+
+# below reads in data using Pandas and creates Numpy array of data
+data = pd.read_csv("iris.csv", header=-1) # note I set header =-1 as data in row 0
+A = np.array (data) # created a NumpPy array of the dataset
+
+## Script below allows anyone running script to decide whether to save the print output to file or display in terminal
+## if D is selected output will display on terminal
+## if S is selected output will be saved to a file
+## any other or no selection will result in a display on terminal
 
 while True:
     response = input("Display or save to file? D or S > ")
@@ -18,23 +31,21 @@ while True:
         sys.stdout=open ('generalstats.txt','w') # redirected to file
         break###turns off interactieve display
     elif response == "D":break    
+    elif "D":break
 
-plt.style.use('seaborn-whitegrid')
-
-data = pd.read_csv("iris.csv", header=-1) ## reading in data using pandas
-# note I set header =-1 as data in row 0
-A = np.array (data) # created a NumpPy array of the dataset
-
+    
 sum_col = (np.sum(A[:,0]))
 avg_col = sum_col/150
 
-print (data)  # printed the dataset 
+print (data)  # This printed the dataset 
 
 ## used print statements to create empty lines, single, double and multiples
 
 print('\n'*2)
 
+
 ## below are some basic stats for all species
+print ('SIMPLE STATISTICS FOR ALL MEASUREMENTS')
 print ("Minimum Sepal length all species is",np.min(A[:,0]),"cm")
 print ("Maximum Sepal length all species is",np.max(A[:,0]),"cm")
 print ("Mean Sepal length all species is", np.mean(A[:,0]),"cm")
@@ -53,7 +64,8 @@ print ('Mean Petal width all species is', np.mean(A[:,3]),"cm")
 
 # below produces basic stats, this time grouped by species
 #data.groupby(4)[0].min()
-print ('Statistics Grouped by Species')
+
+print ('SIMPLE STATISTICS GROUPED BY SPECIES')
 
 print('\n')
 print("Minimum sepal length per species" , data.groupby (4) [0].min())
@@ -63,10 +75,9 @@ print('\n')
 print("Mean sepal length per species" , data.groupby (4) [0].mean())
 
 
-
-print('\n') ##insert blank line
-print ("Number of records per species")
 ##below prints number of records per species
+print('\n') 
+print ("Number of records per species")
 print (data[4].value_counts())
 
 print('\n')
@@ -74,9 +85,13 @@ print('\n')
 # below uses the describe function to produce some statistics of the Iris dataset
 # it is displayed and also saved to csv file summary.csv
 
-summary = data.groupby(4)[0].describe()
-
 print('\n')
+
+# below uses the describe function to produce some general statistics of the Iris dataset
+# it is displayed and also saved to csv file summary.csv
+
+print ('STATISTICS USING DESCRIBE FUCTION - output displayed and saved to summary.csv')
+summary = data.groupby(4)[0].describe()
 print (" Summary of data set", summary)
 summary.to_csv('summary.csv')
 
@@ -125,4 +140,28 @@ plt.scatter(x, y)
 plt.savefig ('Petal.png')
 plt.show ()
 
+PetalWidth1 = 0
+PetalWidth2 = 0
+Irisvirginicacount = 0
+multicond = 0
 
+
+i=0
+for i in range (-1,149):
+    if A[i][3] == 1:
+        PetalWidth1 = PetalWidth1 + 1
+    if  A[i][3] == 2:
+        PetalWidth2 = PetalWidth2 + 1
+
+    if  ((A[i][3] > 1.5) and (A[i][3] < 2.5) and (A[i][4] == 'Iris-virginica')):
+        Irisvirginicacount = Irisvirginicacount +1
+
+    #if (((A[i][0] < 6) and (A[i][2]>=4)and (A[i][4] == 'Iris-versicolor')
+     #   or if ((A[i][3] > 1.5) and (A[i][3] < 2.0) and (A[i][4] == 'Iris-virginica'))):
+      #  multicond = multicond +1
+       # continue
+
+print ("Number of flowers with Petal Width 1cm", PetalWidth1)
+print ("Number of flowers with Petal Width 2cm", PetalWidth2)
+print ("Number of Iris-virginica >1.5 and <2.5 cm", Irisvirginicacount)
+#print ("multi cond", multicond)
